950af498b8884c1d41fc4e0c35a3de71ba9be7a5
Bemmagz <bemmagz@gmail.com>
2025-12-22 04:18:05 +0700

    Full Modular Launch 2025-12-22

---
+++ MODULES/tokenomics.py
@@ -0,0 +1,6 @@
+{
+    "project": "indienation-neurosphere",
+    "ENPE": {"supply": "100T", "donation": "NONE", "staking": "20%"},
+    "LUV": {"supply": "100T", "donation_pool": "15% OPEN", "reward": "1M each for 1M people"},
+    "locking": {"founder": "3 YEARS", "others": "2 YEARS", "date": "2025-12-20"}
+}
