32c8251b9fe0786aa95dc5b4b2f29c64b6719f7f
INDIE-Founder <mysongtour@gmail.com>
2026-02-04 13:17:36 +0700

    Logic: Add gateway validation and improve QR scan-ability

---
+++ gateway.js
@@ -0,0 +1,7 @@
+function validateFounder(data) {
+    if (data === "INDIE-0000000001") {
+        console.log("Akses Diterima: Selamat Datang INDIE-Founder");
+        return true;
+    }
+    return false;
+}
